42e8b863ccc76120e968ef37bd02ac7f09631f58
SnejPro <11321022+SnejPro@users.noreply.github.com>
2026-07-20 11:51:38 +0200

    - add: monitor single faulted disks - add: performance data on disks and pools
    Signed-off-by: SnejPro <11321022+SnejPro@users.noreply.github.com>

--- check_zpools.sh
+++ check_zpools.sh
@@ -75,6 +75,14 @@ then
     echo "UNKNOWN: zpool not found in path: $PATH, please check if command exists and PATH is correct"
     exit "$STATE_UNKNOWN"
 fi
+
+# Check if jq is available
+if ! which jq 1>/dev/null
+then
+    JQ_AVAILABLE=0
+else
+    JQ_AVAILABLE=1
+fi
 #########################################################################
 # Check for people who need help - we are nice ;-)
 if [[ $1 == "--help" || "${#}" == "0" ]]
@@ -171,6 +179,63 @@ do
         fcrit=1
     fi
 
+    # check if disks are healthy
+    # don't break existing installations which don't have jq installed
+    if [[ $JQ_AVAILABLE == 1 ]]
+    then
+        POOL_STATUS_JSON=$(zpool status "${POOLS[$p]}" -j --json-int)
+
+        check_vdev () {
+            VDEV_TYPE=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.vdev_type")
+            if [[ $VDEV_TYPE != null ]]
+            then
+                if [[ $VDEV_TYPE == "disk" ]]
+                then
+                    DISK_HEALTH=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.state")
+                    DISK_NAME=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.name")
+
+                    DISK_READ_ERRORS=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.read_errors")
+                    DISK_WRITE_ERRORS=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.write_errors")
+                    DISK_CHECKSUM_ERRORS=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.checksum_errors")
+
+                    perfdata+=("${POOLS[$p]}---disk---${DISK_NAME}---READ-ERRORS=${DISK_READ_ERRORS}")
+                    perfdata+=("${POOLS[$p]}---disk---${DISK_NAME}---WRITE-ERRORS=${DISK_WRITE_ERRORS}")
+                    perfdata+=("${POOLS[$p]}---disk---${DISK_NAME}---CHECKSUM-ERRORS=${DISK_CHECKSUM_ERRORS}")
+
+                    if [[ $DISK_HEALTH != "ONLINE" ]]
+                    then
+                        error["$p"]+="POOL ${POOLS[$p]} has unhealthy disk ${DISK_NAME} with state $DISK_HEALTH"
+                        fcrit=1
+                    fi
+                else
+                    DISK_NAME=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.name")
+
+                    DISK_READ_ERRORS=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.read_errors")
+                    DISK_WRITE_ERRORS=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.write_errors")
+                    DISK_CHECKSUM_ERRORS=$(echo "${POOL_STATUS_JSON}" | jq -r "${1}.checksum_errors")
+
+                    perfdata+=("${POOLS[$p]}---${VDEV_TYPE}---${DISK_NAME}---READ-ERRORS=${DISK_READ_ERRORS}")
+                    perfdata+=("${POOLS[$p]}---${VDEV_TYPE}---${DISK_NAME}---WRITE-ERRORS=${DISK_WRITE_ERRORS}")
+                    perfdata+=("${POOLS[$p]}---${VDEV_TYPE}---${DISK_NAME}---CHECKSUM-ERRORS=${DISK_CHECKSUM_ERRORS}")
+                fi
+            fi
+
+            if ! $(echo "${POOL_STATUS_JSON}" | jq "${1} | has(\"vdevs\")")
+            then
+                return 0
+            fi
+
+            VDEV_PATH="${1}.vdevs"
+            VDEVS=$(echo "${POOL_STATUS_JSON}" | jq "${VDEV_PATH} | keys")
+
+            while read -r VDEV; do
+                check_vdev "${VDEV_PATH}.${VDEV}"
+            done < <(jq -c '.[]' <<< "$VDEVS")
+        }
+
+        check_vdev ".pools.${POOLS[$p]}"
+    fi
+
     # Check that capacity is with thresholds
     if (( CAPACITY > crit ))
     then
@@ -187,7 +252,7 @@ do
         error["$p"]+="POOL ${POOLS[$p]} has $SPARES_INUSE spare(s) in use"
     fi
 
-    perfdata["$p"]="${POOLS[$p]}=${CAPACITY}%"
+    perfdata+=("${POOLS[$p]}=${CAPACITY}%")
 done
 
 if (( ${#error[*]} > 0 ))
