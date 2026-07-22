001c51e74b112ea72226787c7557159163a15e45
Napsty <ck@claudiokuenzler.com>
2026-07-22 08:00:23 +0200

    Adjust github workflow to run with installed jq as well

--- tests/test_ok.sh
+++ tests/test_ok.sh
@@ -1,37 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
-zpool status
-
-zpool status -j
-
 echo "Testing health of a single ZFS pool"
-
+set +e
 OUTPUT=$(./check_zpools.sh -p testpool -w 80 -c 90)
-
+set -e
 RET=$?
-
-
 echo "$OUTPUT"
-
-
 if [ "$RET" -ne 0 ]; then
     echo "Expected OK"
     exit 1
 fi
 
-
 echo "Testing ALL ZFS pools"
-
-
+set +e
 OUTPUT=$(./check_zpools.sh -p ALL -w 80 -c 90)
-
+set -e
 RET=$?
-
-
 echo "$OUTPUT"
-
-
 if [ "$RET" -ne 0 ]; then
     echo "Expected OK for ALL"
     exit 1
